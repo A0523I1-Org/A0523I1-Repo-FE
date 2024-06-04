74b0afd3a63f9a8cc930dc04123fd7394ce46763
Phan Phùng <phanvanphung6102001@gmail.com>
2024-06-04 20:28:03 +0700

    push code 6/4:8:27

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -9,46 +9,43 @@ import {useEffect, useState} from "react";
 
 function DefaultLayout ({children})  {
     const location = useLocation();
-    const listNameLocation = ['contract','customer','employee','landing'];
     const [locationPresent,setLocationPresent] = useState("");
-    const [locationPresent1,setLocationPresent1] = useState("");
-
+    const [locationPresentOperation,setLocationPresentOperation] = useState("");
 
     useEffect(() => {
         handleLocation();
-    },[location])
+    },[location.pathname])
 
     const handleLocation = ()=>{
-        const parts = location.pathname.split('/');
-        const firstPart = parts[1]; // "employee"
-        const secondPart = parts[2]; // "create-employee"
-    
-        listNameLocation.forEach(e => {
-            if(firstPart === e && firstPart === "contract" ){
-                setLocationPresent('Hợp đồng')
-            }
-            if(firstPart === e && firstPart === "customer" ){
-                setLocationPresent('Khách hàng')
-            }
-            if(firstPart === e && firstPart === "employee" ){
-                setLocationPresent('Nhân viên')
-            }
-            if(firstPart === e && firstPart === "landing" ){
-                setLocationPresent('Mặt bằng')
-            }
-        })
+        const partsLocation = location.pathname.split('/');
 
-       if(secondPart !== undefined){
-           if(secondPart !== "" && secondPart.split('-')[0] === "create"){
-               setLocationPresent1("Thêm mới")
-           }
-           if(secondPart !== "" && secondPart.split('-')[0] === "edit"){
-               setLocationPresent1("Cập nhật")
-           }
-           if(secondPart !== "" && secondPart.split('-')[0] === "register"){
-               setLocationPresent1("Chi tiết")
-           }
-       }
+        const firstPartLocation = partsLocation[1];
+        const secondPartLocation = partsLocation[2];
+
+        const locationMapping = {
+            contract: 'Hợp đồng',
+            customer: 'Khách hàng',
+            employee: 'Nhân viên',
+            landing: 'Mặt bằng'
+        };
+
+        if(locationMapping[firstPartLocation]){
+            setLocationPresent(locationMapping[firstPartLocation]);
+        }
+
+        if(secondPartLocation){
+            const action = secondPartLocation.split('-')[0];
+            const actionMapping = {
+                create: 'Thêm mới',
+                edit: 'Cập nhật',
+                register: 'Chi tiết'
+            };
+            if (actionMapping[action]) {
+                setLocationPresentOperation(actionMapping[action]);
+            }
+        }else {
+            setLocationPresentOperation("");
+        }
     }
 
     return (
@@ -66,11 +63,13 @@ function DefaultLayout ({children})  {
                         <div className="">
                             <Header/>
                         </div>
-                        <div className="mx-10 overflow-auto bg-[#f1f2f2] ">
+                        <div className="mx-10 overflow-auto mt-5">
                             <div className="w-full h-[40px]  relative">
-                                <div
-                                    className="absolute right-0 w-auto bg-gray-200 h-full flex items-center gap-2 justify-center">
-                                    <span className={'pl-3'}>Trang chủ</span>
+                                <div className="absolute py-2 left-0 top-2.5">
+                                    <FooterExtra nameLocation={location.pathname.trim()}/>
+                                </div>
+                                <div className="absolute right-0 w-auto bg-gray-200 h-full flex items-center gap-2 justify-center">
+                                    <span className={'pl-3 text-yellow-600'}>Trang chủ</span>
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                              strokeWidth="1.5" stroke="currentColor" className="size-4">
@@ -78,8 +77,8 @@ function DefaultLayout ({children})  {
                                               d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                                         </svg>
                                     </span>
-                                    <span className={`${locationPresent1 === "" ? 'pr-3' : ''}`}>{locationPresent}</span>
-                                    {locationPresent1 !== "" ?
+                                    <span className={`${locationPresentOperation === "" ? 'pr-3' : ''}`}>{locationPresent}</span>
+                                    {locationPresentOperation !== "" ?
                                             <span>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                      strokeWidth="1.5" stroke="currentColor" className="size-4">
@@ -89,19 +88,16 @@ function DefaultLayout ({children})  {
                                             </span>
                                         : ""
                                     }
-                                    {locationPresent1 !== "" ?
-                                        <span className={'pr-3'}>{locationPresent1}</span>
+                                    {locationPresentOperation !== "" ?
+                                        <span className={'pr-3'}>{locationPresentOperation}</span>
                                         : ""
                                     }
 
                                 </div>
                             </div>
-                            <div className="h-auto ">
+                            <div className="h-auto mt-5 bg-red-500">
                                 {children}
                             </div>
-                        </div>
-                        <div className="mx-10 footer-extra">
-                            <FooterExtra/>
                         </div>
                     </div>
                 </div>
