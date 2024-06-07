e79a808b8ecbf5788867b3607bf9560f082eb4f6
DienTon <thatdien295@gmail.com>
2024-06-07 20:36:10 +0700

    up

--- src/components/landing/ListLanding.js
+++ src/components/landing/ListLanding.js
@@ -22,6 +22,13 @@ const ListLanding = () => {
 
 
     })
+    const removeCheckedCheckboxes = () => {
+        var checked = document.querySelectorAll(".delete-checkbox:checked");
+        checked.forEach((elem) => {
+          var id = elem.parentElement.dataset.id; // Lấy ID từ thuộc tính data-id của phần tử cha
+          landingService.deleteLandingById(id); // Gọi phương thức xóa với ID tương ứng
+        });
+      }
 
     useEffect(() => {
 
