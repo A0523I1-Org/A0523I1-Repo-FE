de646259568b1f605f976a65ee8e527837f5ba69
lechithien0901 <117095269+lechithien0901@users.noreply.github.com>
2024-06-07 20:44:19 +0700

    up

--- src/components/landing/ListLanding.js
+++ src/components/landing/ListLanding.js
@@ -154,10 +154,7 @@ const ListLanding = () => {
                     <div className="id-button flex gap-3">
                         <button className=" bg-[#4CAF50] h-[36px]"><span className="text-white text-[14px] font-normal">Thêm mới</span>
                         </button>
-                        <button className="bg-[#f44336] h-[36px]"><span
-                            className="text-white text-[14px]">Xóa tất cả</span></button>
-
-
+                
                         <select
                             className="h-[36px] w-[80px] border-[#2196e3]"
                             name="nameFloor"
@@ -326,11 +323,25 @@ const ListLanding = () => {
                     </table>
                 </div>
             </div>
-            <ResponsivePagination
-                total={totalPages}
-                current={currentPage}
-                onPageChange={page => handlePageChange(page)}
-            />
+            <div className=" h-[40px] my-5 relative mx-16 flex  ">
+                <button className="left-0 relative w-[40px] h-[40px] bg-red-500 flex items-center justify-center rounded-full">
+                    <span className="text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </span>
+                    <span className="absolute top-0 right-[-5px] w-[15px] h-[15px] text-black font-bold text-[10px]">10</span>
+                </button>
+                <div className="absolute h-full  right-0 ">
+                    <ResponsivePagination
+            
+                    total={totalPages}
+                    current={currentPage}
+                    onPageChange={page => handlePageChange(page)}
+                />
+                </div>
+            </div>
+           
 
         </>
     );
