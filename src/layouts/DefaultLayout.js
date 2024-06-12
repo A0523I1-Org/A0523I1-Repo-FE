9cdcf5cec0826555f159d1814de301b10b9a97f9
Tran Le Quang Trieu <trieutran025@gmail.com>
2024-06-12 20:53:40 +0700

    trieuTLQ

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -79,7 +79,7 @@ function DefaultLayout ({children})  {
                                     </span>
                                     <span className={`${locationPresentOperation === "" ? 'pr-3' : ''}`}>{locationPresent}</span>
                                     {locationPresentOperation !== "" ?
-                                        <span>
+                                            <span>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                      strokeWidth="1.5" stroke="currentColor" className="size-4">
                                                 <path strokeLinecap="round" strokeLinejoin="round"
