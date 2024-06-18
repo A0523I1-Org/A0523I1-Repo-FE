c0d3d9b1b4cbde2f51c4a7cc0b5d81e5c2fb260a
Phan Phùng <phanvanphung6102001@gmail.com>
2024-06-18 19:57:38 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-=======
-=======
->>>>>>> List/CreateContract-HoaiNT
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
->>>>>>> f251697f32330ced84df97760bd60393a2eea290
 import './App.css';
-import {privateRoutes, publicRoutes} from './routes/router';
+import { privateRoutes,publicRoutes } from './routes/router';
 import DefaultLayout from './layouts/DefaultLayout';
-<<<<<<< HEAD
-import {Fragment} from 'react';
+import { Fragment } from 'react';
 import NotFound from './pages/NotFound';
 import {ToastContainer} from "react-toastify";
-=======
-import React, { Fragment } from 'react';
-import NotFound from './pages/NotFound';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {AdminProvider} from "./services/Authenticate/AdminContext";
->>>>>>> List/CreateContract-HoaiNT
 
 
 function App() {
-<<<<<<< HEAD
     return (
         <>
             <ToastContainer/>
             <BrowserRouter>
                 <Routes>
                     {
-                        publicRoutes.map((route, index) => {
+                        publicRoutes.map((route,index) => {
                             const Layout = route.layuot === null ? Fragment : DefaultLayout;
                             const Page = route.component;
                             return <Route
                                 key={index}
                                 path={route.path}
-                                element={
+                                element = {
                                     <Layout>
                                         <Page/>
                                     </Layout>
@@ -47,14 +31,14 @@ function App() {
                     }
 
                     {
-                        privateRoutes.map((route, index) => {
+                        privateRoutes.map((route,index) => {
                             const Layout = route.layuot === null ? Fragment : DefaultLayout;
                             const id = route.id === ':id' ? route.id : '';
                             const Page = route.component;
-                            return <Route
+                            return  <Route
                                 key={index}
-                                path={route.path + id}
-                                element={
+                                path={route.path+id}
+                                element = {
                                     <Layout>
                                         <Page/>
                                     </Layout>
@@ -63,73 +47,11 @@ function App() {
                         })
                     }
 
-                    <Route path='*' element={<NotFound/>}/>
-
-
+                    <Route path='*' element = {<NotFound/>}/>
                 </Routes>
             </BrowserRouter>
         </>
     );
-=======
-  return (
-   <>
-<<<<<<< HEAD
-       <ToastContainer/>
-        <BrowserRouter>
-=======
-       <AdminProvider>
-       <BrowserRouter>
->>>>>>> List/CreateContract-HoaiNT
-          <Routes>
-            {
-              publicRoutes.map((route,index) => {
-                const Layout = route.layuot === null ? Fragment : DefaultLayout;
-                const Page = route.component;
-                 return <Route
-                 key={index}
-                 path={route.path}
-                 element = {
-                  <Layout>
-                      <Page/>
-                  </Layout>
-                 }
-                 />
-              })
-            }
-
-            {
-              privateRoutes.map((route,index) => {
-                const Layout = route.layuot === null ? Fragment : DefaultLayout;
-                const id = route.id === ':id' ? route.id : '';
-                const Page = route.component;
-                 return  <Route
-                 key={index}
-                 path={route.path+id}
-                 element = {
-                  <Layout>
-                     <Page/>
-                  </Layout>
-                 }
-                 />
-              })
-            }
-
-            <Route path='*' element = {<NotFound/>}/>
-<<<<<<< HEAD
-          </Routes>
-       </BrowserRouter>
-=======
-
-
-      </Routes>
-   </BrowserRouter>
-   <ToastContainer/>
-        </AdminProvider>
-
->>>>>>> List/CreateContract-HoaiNT
-   </>
-  );
->>>>>>> f251697f32330ced84df97760bd60393a2eea290
 }
 
 export default App;
