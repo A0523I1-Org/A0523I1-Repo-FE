27232292391b9e9baf7c461f19951e6bc3569df0
Phan Phùng <phanvanphung6102001@gmail.com>
2024-06-18 19:51:23 +0700

    a

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -11,66 +11,32 @@ function DefaultLayout ({children})  {
 
     return (
         <>
-            {location.pathname === "/"
-                ?
-                <div className={"overflow-hidden w-full h-auto  relative"}>
-                    <Header/>
-                    {children}
-                    <Footer/>
-                </div>
-                :
-                <div className=" h-full ">
-                    <div className=" h-screen custom-grid grid-rows-3">
-                        <div className="">
-                            <Header/>
-                        </div>
-<<<<<<< HEAD
-                        <div className="mx-10 mt-5">
-                            <FooterExtra nameLocation={location.pathname}/>
-                            <div className="h-auto mt-5 bg-red-500">
-=======
-                        <div  className="mx-10 overflow-auto mt-5 ">
-                            
-                            <div className="w-full h-[40px]  relative">
-                                <div className="absolute py-2 left-0 top-2.5">
-                                    <FooterExtra nameLocation={location.pathname.trim()}/>
-                                </div>
-                                <div className="absolute right-0 w-auto bg-gray-200 h-full flex items-center gap-2 justify-center">
-                                    <span className={'pl-3 text-yellow-600'}>Trang chủ</span>
-                                    <span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             strokeWidth="1.5" stroke="currentColor" className="size-4">
-                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                              d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
-                                        </svg>
-                                    </span>
-                                    <span className={`${locationPresentOperation === "" ? 'pr-3' : ''}`}>{locationPresent}</span>
-                                    {locationPresentOperation !== "" ?
-                                            <span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                     strokeWidth="1.5" stroke="currentColor" className="size-4">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
-                                                </svg>
-                                            </span>
-                                        : ""
-                                    }
-                                    {locationPresentOperation !== "" ?
-                                        <span className={'pr-3'}>{locationPresentOperation}</span>
-                                        : ""
-                                    }
-
-                                </div>
+            return (
+            <>
+                {location.pathname === "/"
+                    ?
+                    <div className={"overflow-hidden w-full h-auto bgg-red-500"}>
+                        <Header/>
+                        {children}
+                        <Footer/>
+                    </div>
+                    :
+                    <div className=" h-full ">
+                        <div className=" h-screen custom-grid grid-rows-3">
+                            <div className="">
+                                <Header/>
                             </div>
-
-                            <div className="h-auto mt-5 ">
->>>>>>> List/CreateContract-HoaiNT
-                                {children}
+                            <div className="mx-10 mt-5">
+                                <FooterExtra nameLocation={location.pathname}/>
+                                <div className="h-auto mt-5 bg-red-500">
+                                    {children}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            }
+                }
+            </>
+            )
         </>
     )
 }
